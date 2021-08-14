8067f1550b57e16db87acd78adcd97aa85c03f0e
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-14 12:01:41 +0530

    [Vaibhav] added length of lines are calculated

--- src/com/bridgelabz/lincomparison/LineComparison.java
+++ src/com/bridgelabz/lincomparison/LineComparison.java
@@ -4,6 +4,10 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 			System.out.println("Welcome to line comparison computation program");
+			int x1=12,y1=14,x2=23,y2=43,x3=21,y3=32,x4=65,y4=96;
+			int lineOneLength=(int) Math.sqrt((y1-x1)^2+(y2-x2)^2);
+			int lineTwoLength=(int) Math.sqrt((y3-x3)^2+(y4-x4)^2); //UC1
+			System.out.println(lineOneLength+"  "+lineTwoLength);
 	}
 
 }
