ebe55c65f05b1501ba9b6227572d1308b999597b
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-14 11:58:21 +0530

    [Vaibhav] added welcome messege printed

---
+++ src/com/bridgelabz/lincomparison/LineComparison.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.lincomparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+			System.out.println("Welcome to line comparison computation program");
+	}
+
+}
